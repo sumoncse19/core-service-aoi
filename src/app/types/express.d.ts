4b148971f3c1af56568d209c7675f9fa4e82d048
sumoncse19 <mdsumoncse19@gmail.com>
2024-11-07 06:34:14 +0600

    update express type file

--- src/app/types/express.d.ts
+++ src/app/types/express.d.ts
@@ -1,4 +1,65 @@
 import { Request } from 'express'
+import { ClerkUser } from '@clerk/clerk-sdk-node'
+import { RequestHandler } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { ParsedQs } from 'qs'
+import { UserRole } from '../modules/shared/enumeration'
+
+export interface AuthRequest extends Request {
+  auth?: ClerkUser
+  user?: {
+    userId: string
+    role: string
+    user_name: string
+  }
+}
+
+export interface RequestWithAuth extends Request {
+  auth?: {
+    userId: string
+    roles: UserRole[]
+    user_name: string
+  }
+}
+
+export type AuthRequestHandler<
+  P = ParamsDictionary,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = ParsedQs,
+> = RequestHandler<P, ResBody, ReqBody, ReqQuery>
+
+/**
+ * import { Request } from 'express'
+import { ClerkUser } from '@clerk/clerk-sdk-node'
+import { RequestHandler } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { ParsedQs } from 'qs'
+
+export interface AuthRequest extends Request {
+  auth?: {
+    userId: string
+    role: string
+    user_name: string
+    name?: string
+    email?: string
+  }
+  user?: ClerkUser
+}
+
+export type AuthRequestHandler<
+  P = ParamsDictionary,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = ParsedQs,
+> = RequestHandler<P, ResBody, ReqBody, ReqQuery>
+
+ */
+
+// New code will be this for child unit test:
+/**
+ * 
+import { Request } from 'express'
 import { RequestHandler } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { ParsedQs } from 'qs'
@@ -38,3 +99,5 @@ declare global {
     }
   }
 }
+
+ */
